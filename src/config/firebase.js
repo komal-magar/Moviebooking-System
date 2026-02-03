52a5442648a86ac8093194849d44fa59d14af3d4
komal-magar <komalmagar560@gmail.com>
2026-02-03 23:35:37 +0545

    Change Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,21 @@
-// src/config/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-Uzw3IGT0sX8Xbo63xVwJmuWjEizSHvA",
-  authDomain: "ecommerce-3c2f5.firebaseapp.com",
-  projectId: "ecommerce-3c2f5",
-  storageBucket: "ecommerce-3c2f5.appspot.com",
-  messagingSenderId: "965393234207",
-  appId: "1:965393234207:web:1d648517a3362573e7182a",
-  measurementId: "G-2ZGGKHZ8PZ"
+  apiKey: "AIzaSyA3eDENur-hsbSYSeyFucnFl0iiHxXx2mA",
+  authDomain: "moviebooking-edc15.firebaseapp.com",
+  projectId: "moviebooking-edc15",
+  storageBucket: "moviebooking-edc15.firebasestorage.app",
+  messagingSenderId: "1003793533314",
+  appId: "1:1003793533314:web:85416963320f430ebf679b",
+  measurementId: "G-XNHXR71M5Q"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export { app, analytics };
